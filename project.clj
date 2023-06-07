98fe3811caa3de2a8674cc2f54ab37a4b9942d27
jonerik <jonerik@gmail.com>
2023-06-07 11:06:14 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject cljtime "0.9.0-SNAPSHOT"
+  :description "UNIX Time Tools in Pure Clojure"
+  :url "http://lavagoatsoftware.com/clljtime"
+  :license {:name "GPL-3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot cljtime.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})

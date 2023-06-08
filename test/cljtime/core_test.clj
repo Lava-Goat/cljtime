707d4630144d82c5d79a9314744d0de5d91bb66e
Jon-Erik G. Storm <jonerik@gmail.com>
2023-06-07 20:30:09 -0700

    Tests.

--- test/cljtime/core_test.clj
+++ test/cljtime/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [cljtime.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(let [unixTime (quot (System/currentTimeMillis) 1000)]
+(timeMap unixTime))
+
+(let [now (timeMap (- (quot (System/currentTimeMillis) 1000) (* 7 3600)) "Español")]
+(str (now :day)
+      " el "
+     (now :dayOfTheMonth) 
+      " de " 
+     (now :month-lang) 
+      ", " 
+     (now :year) 
+      ". " 
+     (now :12hr)
+      ":" 
+     (now :min)
+      ":" 
+     (now :sec)
+      " " 
+     (now :ampm)))
+(unix->iso8601 (quot (System/currentTimeMillis) 1000))
+(unix->rfc3339 (quot (System/currentTimeMillis) 1000))

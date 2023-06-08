b617e0b49460a1b3de207c820108a2d79f847cf4
Jon-Erik G. Storm <jonerik@gmail.com>
2023-06-07 20:02:05 -0700

    Style and idiom.

--- project.clj
+++ project.clj
@@ -8,3 +8,4 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :plugins [[dev.weavejester/lein-cljfmt "0.10.4"]]
